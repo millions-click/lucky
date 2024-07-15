6bef9c19125fb4bec41fbce1bc97fa3492f7ee14
👻 <y@uklok.ai>
2024-07-14 23:29:21 -0400

    feat(web): 👻 Stored Key | Crypto
    - The key is current stored as plain encoded text in the localstorage.
    - Is loaded on init and context provide the crypto with the loaded key.
    - An update key function is provided to properly update the context and storage.

--- web/providers/crypto-provider.tsx
+++ web/providers/crypto-provider.tsx
@@ -1,16 +1,56 @@
 'use client';
 
-import { createContext, useContext, useMemo } from 'react';
+import { createContext, useContext, useState } from 'react';
 import { Crypto } from '@/utils';
+import { atomWithStorage } from 'jotai/utils';
+import { atom, useAtomValue, useSetAtom } from 'jotai/index';
 
-const Context = createContext<Crypto>(new Crypto());
+export type CryptoState = 'unsafe' | 'safe' | 'expired';
+type CryptoContext = {
+  state: CryptoState;
+  crypto: Crypto;
+  updateKey: (key: string, ttl?: number) => void;
+};
+
+const keyAtom = atomWithStorage<string | undefined>(
+  'lb-key',
+  undefined,
+  undefined,
+  {
+    getOnInit: true,
+  }
+);
+const activeKeyAtom = atom<string | undefined>((get) => {
+  const encrypted = get(keyAtom);
+  if (!encrypted) return;
+
+  const crypto = new Crypto();
+  return crypto.decrypt(encrypted);
+});
+
+const Context = createContext({
+  state: 'unsafe',
+  crypto: new Crypto(),
+} as CryptoContext);
 
 export function CryptoProvider({ children }: { children: React.ReactNode }) {
-  // TODO: Load key from session storage.
-  const key = undefined;
-  const crypto = useMemo(() => new Crypto(key), [key]);
+  const key = useAtomValue(activeKeyAtom);
+  const setKey = useSetAtom(keyAtom);
 
-  return <Context.Provider value={crypto}>{children}</Context.Provider>;
+  const [crypto, setCrypto] = useState(new Crypto(key));
+
+  const value: CryptoContext = {
+    state: key ? 'safe' : 'unsafe',
+    crypto,
+    updateKey: (key: string, ttl?: number) => {
+      // TODO: encrypt the key as a JWT token and store it in the local storage.
+      const crypto = new Crypto();
+      setKey(crypto.encrypt(key));
+      setCrypto(new Crypto(key));
+    },
+  };
+
+  return <Context.Provider value={value}>{children}</Context.Provider>;
 }
 
 export function useCrypto() {
