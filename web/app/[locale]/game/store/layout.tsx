911b1d99287d809d82fd22cee72e8ebfcc208729
👻 <y@uklok.ai>
2024-07-21 19:19:31 -0400

    fix(web): 👻 Query Provider
    - The LuckyBag provider now requires to have the query context to perform some actions.

--- web/app/[locale]/game/store/layout.tsx
+++ web/app/[locale]/game/store/layout.tsx
@@ -3,7 +3,6 @@
 import { PropsWithChildren } from 'react';
 
 import {
-  ReactQueryProvider,
   CryptoProvider,
   LuckyBagsProvider,
   LuckyBagProvider,
@@ -15,16 +14,14 @@ const bg = 'store.webp';
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <ReactQueryProvider>
-      <CryptoProvider>
-        <LuckyBagsProvider>
-          <LuckyBagProvider>
-            <Background name={bg} />
-            <div className="max-w-md bg-base-300/20 p-8">{children}</div>
-            <StoreChatController />
-          </LuckyBagProvider>
-        </LuckyBagsProvider>
-      </CryptoProvider>
-    </ReactQueryProvider>
+    <CryptoProvider>
+      <LuckyBagsProvider>
+        <LuckyBagProvider>
+          <Background name={bg} />
+          <div className="max-w-md bg-base-300/20 p-8">{children}</div>
+          <StoreChatController />
+        </LuckyBagProvider>
+      </LuckyBagsProvider>
+    </CryptoProvider>
   );
 }
--- web/providers/bag-provider.tsx
+++ web/providers/bag-provider.tsx
@@ -3,13 +3,16 @@ import { PropsWithChildren } from 'react';
 import { ClusterProvider } from '@/components/cluster/cluster-data-access';
 import { SolanaProvider } from '@/providers/solana-provider';
 import { DataFeedProvider } from '@/providers/data-feed';
+import { ReactQueryProvider } from '@/providers/react-query-provider';
 
 export function LuckyBagProvider({ children }: PropsWithChildren) {
   return (
-    <ClusterProvider>
-      <SolanaProvider>
-        <DataFeedProvider>{children}</DataFeedProvider>
-      </SolanaProvider>
-    </ClusterProvider>
+    <ReactQueryProvider>
+      <ClusterProvider>
+        <SolanaProvider>
+          <DataFeedProvider>{children}</DataFeedProvider>
+        </SolanaProvider>
+      </ClusterProvider>
+    </ReactQueryProvider>
   );
 }
