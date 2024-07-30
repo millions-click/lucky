d77a46a4276fe547d3a7c0e43be0cc1946b31704
👻 <y@uklok.ai>
2024-07-30 19:36:33 -0400

    style(web): 👻 Logo
    - The logo was updated to properly display a correct size on landing nav panel.

--- web/next.config.js
+++ web/next.config.js
@@ -6,7 +6,7 @@ const createNextIntlPlugin = require('next-intl/plugin');
 
 const withPWA = require('next-pwa')({
   dest: 'public', // destination directory for the PWA files
-  // disable: process.env.NODE_ENV === 'development', // disable PWA in the development environment
+  disable: process.env.NODE_ENV === 'development', // disable PWA in the development environment
   register: true, // register the PWA service worker
   skipWaiting: true, // skip waiting for service worker activation
 });
